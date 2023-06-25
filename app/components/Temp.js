e2225b03d7b61138d1a39d4e58eec4e5fbd11220
hoffmanaiden <hoffman.aiden@gmail.com>
2023-06-25 13:26:23 -0400

    changed temp icon

--- app/components/Temp.js
+++ app/components/Temp.js
@@ -1,7 +1,7 @@
 export default function Temp(){
   return(
     <div>
-      <div className='sectionIcon'>🌡</div>
+      <div className='sectionIcon'>🌡️</div>
       <div className='graphContainer'>Graph Container</div>
       <div className='additionalInfo'>Additional Info</div>
     </div>
